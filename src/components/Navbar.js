adf0eb6d72fade56e77b24585261c950e11d2317
Marko Zecevic <marczecevic@gmail.com>
2021-07-21 11:09:22 +0200

    Style hnavbar and footer components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import {BrowserRouter,Link} from 'react-router-dom'
+import '../styles/Navbar.css'
+import { Input } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Navbar() {
     return (
             <BrowserRouter>  
-            <nav>
-                <ul>
+            <nav className='navbar'>
+                <ul className='nav-list'>
                     <div className="meal-logo">
                         <li><Link to='/'>Meal catalogue</Link></li>
                     </div>
                     <div className="list-item">
                         <Link to='/'>Home</Link>
-                        <Link to='/random'>Get random meal</Link>
+                        <Link to='/random'>Random meal</Link>
                         <Link to='/about'>About us</Link>
+                    </div>
+                    <div className="search-item" >
+                    <div className="search-icon"><SearchIcon/></div>
+                    <Input placeholder='search meals' color='primary'/>
                     </div>
                 </ul>
             </nav>
